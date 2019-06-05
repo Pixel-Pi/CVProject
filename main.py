83d1de90a1bc84f41784fbdd828686ec1016f56b
DJPixelPi <derfreund8@gmail.com>
2019-06-05 14:51:36 +0200

    Collected images for nutella and cereals
    Used the images of nutella to create a first model, now collecting the
    other images to complete the dataset for our use-case.

--- main.py
+++ main.py
@@ -53,29 +53,13 @@ from object_detection.utils import visualization_utils as vis_util
 
 
 # What model to download.
-MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
-MODEL_FILE = MODEL_NAME + '.tar.gz'
-DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+MODEL_NAME = './models/research/object_detection/nutellagraph'
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
 PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = os.path.join('./models/research/object_detection/data', 'mscoco_label_map.pbtxt')
-
-
-# ## Download Model
-
-# In[26]:
-
-
-# opener = urllib.request.URLopener()
-# opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-tar_file = tarfile.open(MODEL_FILE)
-for file in tar_file.getmembers():
-    file_name = os.path.basename(file.name)
-    if 'frozen_inference_graph.pb' in file_name:
-        tar_file.extract(file, os.getcwd())
+PATH_TO_LABELS = os.path.join('./models/research/object_detection/training', 'label_map.pbtxt')
 
 
 # ## Load a (frozen) Tensorflow model into memory.
