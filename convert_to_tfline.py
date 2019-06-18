6cb8677d07f040088fc0caa659fa73902c462bd1
DJPixelPi <derfreund8@gmail.com>
2019-06-18 18:35:11 +0200

    Converted shopmodel2 to .tflite format
    Used the official documentation to write a conversion tool. Converted
    shopmodel2 to .tflite file.

---
+++ convert_to_tfline.py
@@ -0,0 +1,38 @@
+'''
+import tensorflow as tf
+
+graph_def_file = ""
+input_arrays = ["input"]
+output_arrays = ["MobilenetV1/Predictions/Softmax"]
+
+converter = tf.lite.TFLiteConverter.from_frozen_graph(
+  graph_def_file, input_arrays, output_arrays)
+tflite_model = converter.convert()
+open("converted_model.tflite", "wb").write(tflite_model)
+'''
+
+import tensorflow as tf
+import os
+
+img = tf.placeholder(name="img", dtype=tf.float32, shape=(1, 64, 64, 3))
+var = tf.get_variable("weights", dtype=tf.float32, shape=(1, 64, 64, 3))
+val = img + var
+out = tf.identity(val, name="out")
+
+MODEL_NAME = 'shopmodel2'
+PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
+PATH_TO_LABELS = os.path.join(MODEL_NAME, 'label_map.pbtxt')
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+
+
+with tf.Session() as sess:
+  sess.run(tf.global_variables_initializer())
+  converter = tf.lite.TFLiteConverter.from_session(sess, [img], [out])
+  tflite_model = converter.convert()
+  open("converted_model.tflite", "wb").write(tflite_model)
